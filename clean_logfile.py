e64cefffc53337fabd26e8bae36b11ccc329d565
KrippeJaevel <kri.karlsson@gmail.com>
2021-12-03 16:35:29 +0100

    Initial work on cleanup + generate charts

--- clean_logfile.py
+++ clean_logfile.py
@@ -1,4 +1,6 @@
-import re
+import sys
+import pandas as pd
+"""
 errors = []
 linenum = 0
 pattern = re.compile("Server load", re.IGNORECASE)  # Compile a case-insensitive regex
@@ -21,3 +23,50 @@ for err in errors:                            # Iterate over the list of tuples
 
     f.write(resemicolon + '\n')
     print(resemicolon)
+"""
+
+    #Time, FPS if player
+    #Time, FPS, local units if not player
+    #Start with: [LOGGING] [STATS]
+
+#########################################
+errors = []
+#Used to differentiate between players and non-players
+#nonPlayers = ["HC", "SERVER"]
+#opName = input("Please enter the filename of the rpt you fancy...:\n")
+#if opName.find(".rpt") < 0:
+#    print("you fucked up")
+#    sys.exit()
+opName = "2021_11_28_CHI.rpt"
+output = open("rpt_cleaned.csv", "w")
+output.write("Server Time,Source,FPS,Local units" + "\n")
+
+def cleanData():
+    logfile = open(opName, "r", errors='replace')
+    for line in logfile:
+        if line.find("[LOGGING] [STATS]") > 0:
+            tmp = line[:line.find(" -  Active Scripts:")]
+            if tmp.find(" - Total units:") > 0:
+                tmp = tmp[:tmp.find(" - Total units:")]
+            time = tmp[:tmp.find(" \"[LOGGING]")]
+            source = tmp[tmp.find("Source: ") + 8:tmp.find(" - FPS:")]
+            fps = tmp[tmp.find("FPS: ") + 5:tmp.find(" - Local groups:")]
+            localUnit = tmp[tmp.find("Local units: ") + 13:]
+            sum = time + "," + source + "," + fps + "," + localUnit + "\n"
+            #print(sum) #sanity check
+            output.write(sum)
+    output.close()
+    return opName
+
+#def createDataset():
+    #introduce logic to differentiate between players and non-players at the cleanData steps
+    #playerMeanFps
+    #serverFps
+    #HCLocalUnits
+    #player = open("playerFps.csv","w")
+    #player.write("Server Time,Source,FPS" + "\n")
+  #  server = open("serverFps.csv","w")
+   # server.write("Server Time,FPS"+ "\n")
+    #HC
+    #for line in output:
+        
--- generate_charts.py
+++ generate_charts.py
@@ -3,56 +3,72 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import altair as alt
-from altair_saver import save
+import clean_logfile as clean
+#from altair_saver import save
 import matplotlib.dates as md
+#import matplotlib.patches as mpatches
 
 
 ####CODE####
+date = clean.cleanData()
 sns.set_style("whitegrid")
-df = pd.read_csv('server_logs_cleaned.csv', header=0)
+df = pd.read_csv('rpt_cleaned.csv', header=0)
 #print(df) #sanity check
 #print(df.dtypes)
 df['Server Time'] = pd.to_datetime(df['Server Time']) #converts string to datetime (YYYY-mm-dd-HH-mm-ss)
 df['Time Diff'] = df['Server Time'].diff()
-m = df['Server Time'].diff() < pd.Timedelta(0)
-df['Server Time'] += pd.to_timedelta(m.cumsum(), unit='d')
+pastMidnight = df['Server Time'].diff() < pd.Timedelta(0)
+df['Server Time'] += pd.to_timedelta(pastMidnight.cumsum(), unit='d')
 #print(df.dtypes)
-print(m)
-print(df)
-df.to_csv('server_logs_panda.csv')
+#print(pastMidnight)
+#print(df['FPS'])
+#df.to_csv('server_logs_panda.csv')
 
-'''
-plt.figure(figsize=(20,10))
-plot1 = sns.lineplot(x="Server Time", y="Server FPS",ci=None, data=df)
-plot1.xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-plt.savefig("Server_FPS.png", dpi=200)
-plt.close()
 
-plt.figure(figsize=(20,10))
-plot2 = sns.lineplot(x="Server Time", y="Player Count",ci=None, data=df)
-plot2.xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-plt.savefig("Player Count.png", dpi=200)
-plt.close()
-
-plt.figure(figsize=(20,10))
-plot3 = sns.lineplot(x="Server Time", y="Guaranteed Messages",ci=None, data=df)
-plot3.xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-plt.savefig("G-Messages.png", dpi=200)
-plt.close()
-'''
-
-figure, axes = plt.subplots(7, 1, sharex=True, figsize=(35,25))
-figure.suptitle('My beautiful Charts')
-#figure.tight_layout()
+figure, axes = plt.subplots(3, 1, figsize=(35,25))
+figure.suptitle("Server stats")
 figure.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.05, hspace=0.05)
-sns.lineplot(ax=axes[0],x="Server Time", y="Server FPS",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[1],x="Server Time", y="Player Count",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[2],x="Server Time", y="Guaranteed Messages",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[3],x="Server Time", y="Non-G Messages",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[4],x="Server Time", y="RAM [MB]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[5],x="Server Time", y="in [Kbps]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
-sns.lineplot(ax=axes[6],x="Server Time", y="out [Kbps]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#TODO: Change array to read from "HC" and "SERVER"tags in data.
+npc = ["Server", "HC1", "HC2"]
+def getPlayers():
+    players = []
+    for name in df["Source"]:
+        #TODO: check why error no vehicle shows up in rpt.
+        if name not in npc and name not in players and name.find("Error") < 0:
+            players.append(name)
+    return players
+
+def plotNpcValues():
+    for src in npc:
+        plotFPS(src)
+        plotLocalUnits(src)
+def plotPlayerFps():
+    players = getPlayers()
+    for player in players:
+        plotFPS(player, 2)
+def plotFPS(source, index=0):
+    sns.lineplot(ax=axes[index], x="Server Time", y="FPS",ci=None, label=source, data=df.loc[df['Source'].isin([source])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+def plotLocalUnits(source):
+    sns.lineplot(ax=axes[1], x="Server Time", y="Local units",ci=None, label=source, data=df.loc[df['Source'].isin([source])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+
+#sns.lineplot(ax=axes[0], x="Server Time", y="FPS",ci=None, label="Server" ,data=df.loc[df['Source'].isin(['Server'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[0], x="Server Time", y="FPS",ci=None, label="HC1", data=df.loc[df['Source'].isin(['HC1'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[0], x="Server Time", y="FPS",ci=None, label="HC2", data=df.loc[df['Source'].isin(['HC2'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[1], x="Server Time", y="Local units",ci=None, label="Server" ,data=df.loc[df['Source'].isin(['Server'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[1], x="Server Time", y="Local units",ci=None, label="HC1", data=df.loc[df['Source'].isin(['HC1'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[1], x="Server Time", y="Local units",ci=None, label="HC2", data=df.loc[df['Source'].isin(['HC2'])]).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+
+#sns.lineplot(ax=axes[0],x="Server Time", y="Server FPS",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[1],x="Server Time", y="Player Count",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[2],x="Server Time", y="Guaranteed Messages",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[3],x="Server Time", y="Non-G Messages",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[4],x="Server Time", y="RAM [MB]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[5],x="Server Time", y="in [Kbps]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+#sns.lineplot(ax=axes[6],x="Server Time", y="out [Kbps]",ci=None, data=df).xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+plotNpcValues()
+plotPlayerFps()
 plt.savefig("Combined.png", dpi=200)
+#plt.show()
 plt.close()
 
 '''
