23e864091b01daebf85a3478661ef133cfee058a
Mildly_Interested <mueller.manuel@mailbox.org>
2021-12-05 12:14:16 +0100

    why are you not ignored!

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+from os import times
 import re
-line = "18:12:08 Server load: FPS 27, memory used: 2487 MB, out: 71436 Kbps, in: 1190 Kbps, NG:0, G:86369, BE-NG:0, BE-G:0, Players: 80 (L:0, R:2, B:1, G:77, D:0)"
-result = re.search('FPS (\d+)', line, re.IGNORECASE)
-
-print(result.group(1))
+#line = "22:03:25 [LOGGING] [STATS] Source: Server - FPS: 61.78 - Local groups: 1 - Local units: 11 - Total units: 112 - Vehicles: 215 -  Active Scripts: [spawn: 15, execVM: 4, exec: 0, execFSM: 2]"
+line = "22:03:28 [LOGGING] [STATS] Source: Sqn Ldr Hendrickx - FPS: 36.87 - Local groups: 1 - Local units: 1 -  Active Scripts: [spawn: 4, execVM: 2, exec: 0, execFSM: 3]"
+fps = line[line.find("FPS: ")+len("FPS: "):line.find(" - Local groups:")]
+source = line[line.find(" Source: ")+len(" Source: "):line.find(" - FPS:")]
+time = line[+1:line.find(" [LOGGING] [STATS]")]
+localUnit = line[line.find("Local units: ")+len("Local units: "):line.find(" ",line.find("Local units: ")+len("Local units: "))]
+print(fps)
+print(source)
+print(time)
+print(localUnit)
