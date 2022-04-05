5c290b862585b88066d06c342dea0a4fd6fec180
Mildly_Interested <mueller.manuel@mailbox.org>
2022-04-05 20:59:01 +0200

    working multi merge renaming all colums so multi merge works
    Co-authored-by: KrippeJaevel <KrippeJaevel@users.noreply.github.com>

--- combine_data.py
+++ combine_data.py
@@ -1,3 +1,4 @@
+from itertools import accumulate
 import pandas as pd
 from functools import reduce
 
@@ -36,6 +37,19 @@ def dict_from_df(df, sources_list, column_name = "Source", HC = False):
             k += 1
     return df_dict
 
+# renaming of the column in order for the merge into a single dataset will work
+#NEEDED FOR UBER MERGER
+def renaming_columns_so_the_shitty_code_wont_break(df_dict):
+    for key in df_dict:
+        print(key)
+        source = "Source_" + key
+        fps =  "FPS_"+key
+        localGroups = "Groups_"+key
+        localUnits =  "Units_"+key
+        df_dict[key] = df_dict[key].rename(columns={"Source": source, "FPS": fps, "Local Groups": localGroups, "Local Units":localUnits})
+    return df_dict
+
+
 def the_uber_merger(left_df, right_df_dict, merge_on = 'Server Time', time_tolerance = 30, merge_direction = 'nearest'):
     #convert right_df_dict to list
     df_list = list(right_df_dict.values())
@@ -44,6 +58,7 @@ def the_uber_merger(left_df, right_df_dict, merge_on = 'Server Time', time_toler
     df_merged = reduce(lambda  left,right: pd.merge_asof(left,right,on=merge_on,tolerance=pd.Timedelta(seconds=time_tolerance), direction=merge_direction), df_list)
     return df_merged
 
+
 server_df = import_csv("server_cleaned.csv")
 player_df = import_csv("player_cleaned.csv")
 hc_df = import_csv("hc_cleaned.csv")
@@ -51,14 +66,3 @@ unique_players = get_unique_sources(player_df)
 unique_hcs = get_unique_sources(hc_df)
 player_dict = dict_from_df(player_df, unique_players)
 hc_dict = dict_from_df(hc_df, unique_hcs, HC = True)
-
-#TODO
-#implement multi merge
-# https://stackoverflow.com/questions/44327999/python-pandas-merge-multiple-dataframes
-#TODO do merge_asof, replace NaN data with zeros afterwards
-# merge_test = pd.merge_asof(server_df, hc_dict["HC_1"],on="Server Time", suffixes=("_server","_HC"), tolerance=pd.Timedelta(seconds=30), direction="nearest")
-# MULTI MERGE
-# df_merged = reduce(lambda  left,right: pd.merge_asof(left,right,on='Server Time',tolerance=pd.Timedelta(seconds=30), direction="nearest"), data_frames)
-# data_frames needs to be list of dfs, the first one will be what will be merged upon so like this [server_df, hc_1_df, hc_2_df, hc_3_df]
-# data_frames = list(hc_dict.values())
-# add server_df to front of list .insert(0, server_df)
