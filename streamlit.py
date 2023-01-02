530cc5a74f9e5289b83041d5f17bf05a8d81124c
MildlyInterested <mueller.manuel@mailbox.org>
2023-01-02 20:09:15 +0100

    working plotting

--- streamlit.py
+++ streamlit.py
@@ -45,15 +45,19 @@ categories = [x for x in categories if x != []]
 
 rowCount = len(categories)
 figtree = make_subplots(rows=rowCount, cols=1,shared_xaxes=True)
+first = []
 for cat in categories:
+    first.append(cat[0])
     for title in cat:
         figtree.add_trace(plotly.graph_objects.Scatter(x=complete_df["Server Time"], y=complete_df[title], mode="lines", name=title), row=categories.index(cat)+1, col=1)
 hover_data = "<br>"
 for i in range(len(filtered)):
     hover_data += filtered[i] + ": "+complete_df[filtered[i]].astype(str) + "<br>"
 hover_data += "<extra></extra>"
+
 figtree.update_layout(hovermode="x unified", height=800, xaxis_rangeslider_visible=True, xaxis_tickformat="%H:%M:%S")
 figtree.update_xaxes(showticklabels=True, showgrid=True)
 figtree.update_traces(xaxis='x1')
-figtree.update_traces(hovertemplate=hover_data, hoverinfo="text")
+for value in first:
+    figtree.update_traces(hovertemplate=hover_data, hoverinfo="text", selector=({'name':value}))
 st.plotly_chart(figtree, use_container_width=True)
