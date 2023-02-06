bf21354958261d6741d2d57a0375da7bad054c1f
MildlyInterested <mueller.manuel@mailbox.org>
2023-02-06 14:31:19 +0100

    working

--- streamlit.py
+++ streamlit.py
@@ -27,6 +27,7 @@ complete_df = calculate.nonplayer_units(complete_df)
 multiselect_list = list(complete_df.columns)
 multiselect_list.remove("Server Time")
 
+#TODO don't rerun whole script if filtered columns are changed
 filtered = st.multiselect("Filter columns", options=multiselect_list, default=["Average Player FPS", "Units on Players", "Units on HC + Server", "Units on all", "Playercount"])
 categories = [[],[],[],[],[]]
 for column in filtered:
@@ -42,22 +43,28 @@ for column in filtered:
         categories[4].append(column)
 
 categories = [x for x in categories if x != []]
+#flatten categories
+categories_flat = [x for y in categories for x in y]
 
 rowCount = len(categories)
 figtree = make_subplots(rows=rowCount, cols=1,shared_xaxes=True)
 first = []
+
 for cat in categories:
     first.append(cat[0])
     for title in cat:
         figtree.add_trace(plotly.graph_objects.Scatter(x=complete_df["Server Time"], y=complete_df[title], mode="lines", name=title), row=categories.index(cat)+1, col=1)
+#TODO flatten the list categories to show the tooltip in the same order as the rendered traces
+# flatCat = [x for x in categories]
+
 hover_data = "<br>"
-for i in range(len(filtered)):
-    hover_data += filtered[i] + ": "+complete_df[filtered[i]].astype(str) + "<br>"
+for i in range(len(categories_flat)):
+    hover_data += categories_flat[i] + ": "+complete_df[categories_flat[i]].astype(str) + "<br>"
 hover_data += "<extra></extra>"
 
 figtree.update_layout(hovermode="x unified", height=800, xaxis_rangeslider_visible=True, xaxis_tickformat="%H:%M:%S")
 figtree.update_xaxes(showticklabels=True, showgrid=True)
 figtree.update_traces(xaxis='x1')
 for value in first:
-    figtree.update_traces(hovertemplate=hover_data, hoverinfo="text", selector=({'name':value}))
+    figtree.update_traces(hovertemplate=hover_data, hoverinfo="text", selector=dict(name=value))
 st.plotly_chart(figtree, use_container_width=True)
